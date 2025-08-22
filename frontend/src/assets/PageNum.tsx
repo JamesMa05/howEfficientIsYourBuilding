b761574dd2a2d049821093d9a78ea936da628670
SemajWork <jamesma765@gmail.com>
2025-08-21 23:04:07 -0400

    color change

--- frontend/src/assets/PageNum.tsx
+++ frontend/src/assets/PageNum.tsx
@@ -3,13 +3,14 @@ import pageNum from './pageNum.svg'
 
 interface PageNumIconProps{
     page:number;
+    colour:string;
 }
 
-export const PageNumIcon: React.FC<PageNumIconProps> = ({page}) => {
+export const PageNumIcon: React.FC<PageNumIconProps> = ({page,colour}) => {
   return (
     <div style={{display:"grid",gridTemplateColumns:"1fr",gridTemplateRows:"1fr",placeItems:"center"}}>
         <img src={pageNum} alt="" style={{display:"block", width:"48px",height:"48px",gridArea:"1/1"}}/>
-        <span style={{fontFamily:"Permanent Marker",gridArea:'1/1',alignSelf:"center",justifySelf:"center",marginBottom:"15px",fontSize:"20px",color:"white"}}>{page}</span>
+        <span style={{fontFamily:"Permanent Marker",gridArea:'1/1',alignSelf:"center",justifySelf:"center",marginBottom:"15px",fontSize:"20px",color:colour}}>{page}</span>
     </div>
   )
 }
